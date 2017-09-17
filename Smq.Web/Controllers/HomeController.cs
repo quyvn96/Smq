16350dee627ecd23b545ce2018e01ca23212c8ad
quyvn96 <quyvn317@gmail.com>
2017-09-17 13:45:33 +0700

    Layout Page

--- Smq.Web/Controllers/HomeController.cs
+++ Smq.Web/Controllers/HomeController.cs
@@ -26,5 +26,20 @@ namespace Smq.Web.Controllers
 
             return View();
         }
+        [ChildActionOnly]
+        public ActionResult Footer()
+        {
+            return PartialView();
+        }
+        [ChildActionOnly]
+        public ActionResult Header()
+        {
+            return PartialView();
+        }
+        [ChildActionOnly]
+        public ActionResult Category()
+        {
+            return PartialView();
+        }
     }
 }
